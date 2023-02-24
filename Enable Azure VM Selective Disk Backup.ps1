ac1c11528a57340d107eff7a29886add11d668ad
Danidu Weerasinghe <122934950+DaniduWeerasinghe911@users.noreply.github.com>
2023-02-24 15:13:18 +1100

    Update Enable Azure VM Selective Disk Backup.ps1

--- Enable Azure VM Selective Disk Backup.ps1
+++ Enable Azure VM Selective Disk Backup.ps1
@@ -1,25 +1,33 @@
-﻿#enable Selective disk backup
+# Author: Danidu Weerasinghe
+# Date: [Date]
+# Description: Enable selective disk backup for an Azure VM
 
-Connect-AzAccount 
+# Connect to Azure
+Connect-AzAccount
 
+# Get the disks in the resource group
 Get-AzDisk -ResourceGroupName "ResourceGroupName"
 
-$Vm = Get-AzVM -ResourceGroupName "ResourceGroupName" -Name "Servername"
+# Get the VM
+$vm = Get-AzVM -ResourceGroupName "ResourceGroupName" -Name "Servername"
 
+# Display the data disks for the VM
 $vm.StorageProfile.DataDisks
 
-#update the Lun Number
-$disks = (“1”)
-#Set the Target Vault
+# Specify the LUN number to backup
+$disks = "1"
+
+# Set the target recovery services vault
 $targetVault = Get-AzRecoveryServicesVault -ResourceGroupName "ResourceGroupName Recovery Service Vault" -Name "RecoveryServiceVaultName"
 
-#Set the recovery service vault configuraion
+# Set the recovery services vault context
 Set-AzRecoveryServicesVaultContext -Vault $targetVault
 
-#set backup policy
-$pol = Get-AzRecoveryServicesBackupProtectionPolicy -Name “DEV12AM7D”
+# Get the backup policy
+$pol = Get-AzRecoveryServicesBackupProtectionPolicy -Name "DEV12AM7D"
 
-#Enable backup
-Enable-AzRecoveryServicesBackupProtection -Policy $pol -Name “ServerName” -ResourceGroupName “ResourceGroup” -InclusionDisksList $disks -VaultId $targetVault.ID
+# Enable backup for the specified disks
+Enable-AzRecoveryServicesBackupProtection -Policy $pol -Name "ServerName" -ResourceGroupName "ResourceGroup" -InclusionDisksList $disks -VaultId $targetVault.ID
 
-$item= Get-AzRecoveryServicesBackupItem -BackupManagementType “AzureVM” -WorkloadType “AzureVM” -VaultId $targetVault.ID -FriendlyName “LinuxVm01”
+# Get the backup item
+$item = Get-AzRecoveryServicesBackupItem -BackupManagementType "AzureVM" -WorkloadType "AzureVM" -VaultId $targetVault.ID -FriendlyName "LinuxVm01"
