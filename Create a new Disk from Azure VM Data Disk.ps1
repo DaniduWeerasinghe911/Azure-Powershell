f3daf8c12859e3a750617b30215885e4b0b50397
Danidu Weerasinghe <122934950+DaniduWeerasinghe911@users.noreply.github.com>
2023-02-24 15:03:50 +1100

    Update Create a new Disk from Azure VM Data Disk.ps1

--- Create a new Disk from Azure VM Data Disk.ps1
+++ Create a new Disk from Azure VM Data Disk.ps1
@@ -1,18 +1,26 @@
-﻿$resourceGroup = "AUSE-DEV-KIWI-RG"
-$DiskName = "ODC1AP0027"
-$NewDiskName = "ODC1AP0027_OSDISK"
-$SubName = "AZPCM29421001-M-Opal"
+# Author: [Author Name]
+# Date: [Date]
+# Description: This script copies an OS disk to a new managed disk.
 
+# Set the resource group and disk names.
+$resourceGroup = ""
+$DiskName = ""
+$NewDiskName = ""
+
+# Set the subscription name.
+$SubName = ""
+
+# Connect to the Azure account.
 Connect-AzAccount
 
-#! Get source OS Disk information
+# Get source OS disk information.
 Write-Verbose "Get the source OS Disk information: $($VM.StorageProfile.OsDisk.Name)"
 $sourceOSDisk = Get-AzDisk -ResourceGroupName $resourceGroup -DiskName $DiskName
 
-#! Create the managed disk configuration
+# Create the managed disk configuration.
 Write-Verbose "Create the managed disk configuration..."
 $diskConfig = New-AzDiskConfig -SkuName $sourceOSDisk.Sku.Name -Location $sourceOSDisk.Location -DiskSizeGB $sourceOSDisk.DiskSizeGB -SourceResourceId $sourceOSDisk.Id -CreateOption Copy
 
-#! Create the new disk
+# Create the new disk.
 Write-Verbose "Create the new OS disk..."
 $newOSDisk = New-AzDisk -Disk $diskConfig -DiskName $NewDiskName -ResourceGroupName $resourceGroup
